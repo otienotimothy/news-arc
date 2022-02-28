80f7250ad4616d100c61efb80aed08074635cc35
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-28 13:56:31 +0300

    Fetch news sources data from the view function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
+from flask import render_template
 from app import app
+
+from .requests import get_news_sources
 
 @app.route('/')
 def home():
+    # Render a template for the home route
+    news_sources_list = get_news_sources()
+    print(news_sources_list)
     return 'Hello World'
