5ba605eec24393b363032597aa64aa901acba1ec
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-28 07:55:35 +0300

    Add config settings and Instantiate bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+# Initialize Bootstrap
+bootstrap = Bootstrap(app)
+
+from . import views
