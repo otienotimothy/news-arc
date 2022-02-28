82e5fd8281e63fb440e68e7c04f2b6780c64c722
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-01 01:02:02 +0300

    Modify the file to remove flask-bootstrap instantiation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from .config import DevConfig
-from flask_bootstrap import Bootstrap
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
@@ -9,7 +8,5 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-# Initialize Bootstrap
-bootstrap = Bootstrap(app)
 
 from . import views
