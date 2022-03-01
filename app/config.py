cc7d2d992608ca1aaf5b2527364d05ddeab9251b
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-01 09:13:14 +0300

    Add the url for the top headlines for individual news sources

--- app/config.py
+++ app/config.py
@@ -4,6 +4,9 @@ class Config:
     '''
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?category=general&language=en&country=us&apiKey={}'
 
+    TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&from=2022-02-28&sortBy=popularity&apiKey={}'
+
+
 class ProdConfig(Config):
     '''
     Production  configuration child class
