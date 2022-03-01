4d8c4af02342a08a25d554f8b0ea568a48214dbb
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-01 07:14:54 +0300

    Add the news sources list to the render template method to render them to the index.html template

--- app/views.py
+++ app/views.py
@@ -7,5 +7,4 @@ from .requests import get_news_sources
 def home():
     # Render a template for the home route
     news_sources_list = get_news_sources()
-    # print(news_sources_list)
-    return render_template('index.html')
+    return render_template('index.html', news_list = news_sources_list)
