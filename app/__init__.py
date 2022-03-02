2ae03286e12818622270b7e021423e9fe43fae09
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-02 06:28:53 +0300

    Register the blueprint with the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -22,4 +22,9 @@ def create_app(config_name):
     #Creating the app from configurations
     app.config.from_object(config_options[config_name])
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    
     return app
