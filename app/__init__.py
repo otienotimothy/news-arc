891723d70f8978e896eefa5b07398126737a42a9
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-02 06:23:45 +0300

    Modify the app instance to use a factory function instaed

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
+# from flask import Flask
+# from ..config import DevConfig
+
+# # Initialize the app
+# app = Flask(__name__, instance_relative_config=True)
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+
+# from .main import views
+
+
 from flask import Flask
-from ..config import DevConfig
+from config import config_options
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+def create_app(config_name):
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app = Flask(__name__)
 
+    #Creating the app from configurations
+    app.config.from_object(config_options[config_name])
 
-from .main import views
+    return app
